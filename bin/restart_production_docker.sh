a1c7f5a9b81072c1096299aae0cd783ab0ba8c32
bigpie1367 <bigpie1367@gmail.com>
2024-03-20 13:29:31 +0900

    [Fix] 오탈자 수정 및 docker 경로 수정

--- bin/restart_production_docker.sh
+++ bin/restart_production_docker.sh
@@ -1,10 +1,10 @@
-docker-compose -p prod_{{ your_project name }} -f production.yml down
+docker-compose -p prod_{{ your_project_name }} -f production.yml down
 
 if [ "$1" == "build" ]; then
-    docker-compose -p prod_{{ your_project name }} -f production.yml up --build -d
+    docker-compose -p prod_{{ your_project_name }} -f production.yml up --build -d
 
-    docker exec -it prod_{{ your_project name }}_django-1 python /app/manage.py migrate
-    docker exec -it prod_{{ your_project name }}_django-1 python /app/manage.py loaddata initial_data.json
+    docker exec -it prod_{{ your_project_name }}_django-1 python /app/manage.py migrate
+    docker exec -it prod_{{ your_project_name }}_django-1 python /app/manage.py loaddata initial_data.json
 else
-    docker-compose -p prod_{{ your_project name }} -f production.yml up -d
+    docker-compose -p prod_{{ your_project_name }} -f production.yml up -d
 fi
